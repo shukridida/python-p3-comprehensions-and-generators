df12e662a436b2da971a2e389a8fef0b39c6aabd
shukri <didashukri0@gmail.com>
2024-06-06 14:02:13 +0300

    added files

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = [num for num in num_list if num % 2 == 0]
+    return even_list
+
 
 def make_exclamation(sentence_list):
-    pass
+    string_list = [sentence + '!' for sentence in sentence_list]
+    return string_list
+
+sentence = ["Hello", "I'm doing great", "Python is fun"]
+print(make_exclamation(sentence))
